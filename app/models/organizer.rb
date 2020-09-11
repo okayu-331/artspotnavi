c30bdd8273626c86ff13fe505e32efeca8fa10c8
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-11 12:00:36 +0900

    バリデーション設定

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -3,4 +3,11 @@ class Organizer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true, format: { with: /\A.+\z/ } do
+    validates :name
+    validates :company_name
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}+\z/i }
+
 end
