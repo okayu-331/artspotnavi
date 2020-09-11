4ed5262b4184cc1761bbc037e59d1d8cc5a0c8c3
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-11 20:37:19 +0900

    new、createアクションの実装

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -3,5 +3,25 @@ class ExhibitionsController < ApplicationController
   end
   
   def new
+    @exhibition = Exhibition.new
+  end
+
+  def create
+    @exhibition = Exhibition.new(exhibition_params)
+    if @exhibition.save
+      redirect_to root_path
+     else
+      render :new
+    end
+  end
+
+  private
+  def exhibition_params
+    params.require(:exhibition).permit(
+      :title, :subtitle, :venue, :description,
+      :start_date, :end_date, :basic_open_time, :basic_close_time,
+      :close_day, :special_open_day, :admission, :address, :access,
+      :phone_number,:url, :prefecture_id
+    ).merge(organizer_id: current_organizer.id)    
   end
 end
