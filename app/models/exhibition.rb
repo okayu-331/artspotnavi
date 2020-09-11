720665d631220b2a5edc4727734d3f74aae9f881
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-11 22:39:02 +0900

    バリデーションの設定

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -4,5 +4,16 @@ class Exhibition < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
+  
+  with_options presence: true do
+    validates :title
+    validates :venue
+    validates :start_date
+    validates :end_date
+    validates :basic_open_time
+    validates :basic_close_time
+    validates :admission
+    validates :address
+    validates :prefecture_id
+  end
 end
