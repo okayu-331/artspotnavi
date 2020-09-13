0f2f553abee83d0febcc0b524d5259d3e0dc62a7
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-13 14:09:24 +0900

    ログイン状態による遷移先の指定

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -1,6 +1,11 @@
 class OrganizersController < ApplicationController
+  before_action :authenticate_organizer!
+
   def show
     @organizer = Organizer.find(params[:id])
     @exhibitions = @organizer.exhibitions
+    if current_organizer.id != @organizer.id
+      redirect_to root_path
+    end
   end
 end
