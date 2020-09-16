739505cf009f7c0406291a1375ac25963ead89f0
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-16 18:30:15 +0900

    Formオブジェクトの実装

---
+++ app/forms/exhibition_event.rb
@@ -0,0 +1,42 @@
+class ExhibitionEvent
+
+  include ActiveModel::Model
+  attr_accessor :title, :subtitle, :venue, :description,
+  :start_date, :end_date, :basic_open_time, :basic_close_time,
+  :close_day, :special_open_day, :admission, :address, :access,
+  :phone_number,:url, :prefecture_id, :image, :organizer_id, :id
+
+  with_options presence: true do
+    validates :title
+    validates :venue
+    validates :image
+    validates :start_date
+    validates :end_date
+    validates :basic_open_time
+    validates :basic_close_time
+    validates :admission
+    validates :address
+    validates :prefecture_id
+  end
+
+  def save
+    exhibition = Exhibition.create(
+      title: title, subtitle: subtitle, venue: venue, description: description, 
+      start_date: start_date, end_date: end_date, basic_open_time: basic_open_time, basic_close_time: basic_close_time,
+      close_day: close_day, special_open_day: special_open_day, admission: admission, address: address, access: access,
+      phone_number: phone_number, url: url, prefecture_id: prefecture_id, image: image,
+      organizer_id: organizer_id)
+
+      dayarr = []
+      (start_date..end_date).each do |d|
+        dayarr << d
+      end
+      
+      events = []
+      dayarr.each do |day|
+        events << Event.new(:open_date => day, :open_time => basic_open_time, :close_time => basic_close_time, :exhibition_id => exhibition.id)
+      end
+      Event.import events
+  end
+end
+
