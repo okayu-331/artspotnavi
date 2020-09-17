e49ce2fd695f14a86ae9e4a1e669ac0cb8788352
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-17 19:18:41 +0900

    topページでのcalendar.jsエラー修正

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -4,12 +4,17 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
+  const path = location.pathname;
+  const eventRegex = /^\/exhibitions\/\d+\/events$/
 
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, interactionPlugin ],
-    events: 'events.json'
-  });
-
-  calendar.render();
+  if(eventRegex.test(path)) {
+ 
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin, interactionPlugin ],
+      events: 'events.json'
+    });
+    
+    calendar.render();
+  }
 });
 
