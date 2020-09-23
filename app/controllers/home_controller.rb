41e16bef6237297bbf6d36b21d1fe145ea6c4d77
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-23 16:08:41 +0900

    画像１枚目を表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @exhibitions = Exhibition.with_attached_image.limit(6).order("created_at DESC")
+    @exhibitions = Exhibition.with_attached_images.limit(6).order("created_at DESC")
   end
 end
