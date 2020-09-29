552070fe5b488916e29899fc25ca31f661fab3d1
okayu-331 <kuma.fuyuno@gmail.com>
2020-09-29 14:14:38 +0900

    event単体モデルテスト実装

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     open_date { Faker::Date.between(from: '2020-09-01', to: '2020-09-30') }
     open_time { "10:00:00" }
     close_time { "17:00:00" }
-    organizer_id { 1 }
-    exhibition_id { 1 }
+    association :organizer
+    association :exhibition
   end
 end
