b3e3d0f517a41c58f305c08a90c74f4d34ae0acf
okayu-331 <kuma.fuyuno@gmail.com>
2020-10-07 11:23:45 +0900

    basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["ART_BASIC_AUTH_USER"] && password == ENV["ART_BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   protected
   def configure_permitted_parameters
